18dcd9f174beff5cfc287496e40a1adde47e806f
danise.iheme <danise.iheme@optimusbank.com>
2024-03-22 14:58:33 +0100

    refactor: removed controller

--- src/Routes/productRoute.ts
+++ src/Routes/productRoute.ts
@@ -1,13 +1,12 @@
 import express from 'express'
 const router = express.Router()
-import { createProductController, getProductByIdController, getProductsByCategoryController, deleteSingleProductController, updateProductController } from '../Controller/ProductController';
+import { createProduct, getProductById, getAllProductsByCategory, deleteSingleProduct, updateProduct } from '../Services/ProductService';
 import {authenticate, isAdmin} from '../Helpers/AuthHelper'
-import { getAllProductsByCategory, getProductById } from '../Services/ProductService';
 
-router.post('/create', isAdmin, createProductController);
-router.get('/get', authenticate, getProductByIdController)
-router.get('/get', authenticate, getProductsByCategoryController);
-router.delete('/delete', isAdmin, deleteSingleProductController)
-router.put('/update', isAdmin, updateProductController)
+router.post('/create', isAdmin, createProduct);
+router.get('/get', authenticate, getProductById)
+router.get('/get', authenticate, getAllProductsByCategory);
+router.delete('/delete', isAdmin, deleteSingleProduct)
+router.put('/update', isAdmin, updateProduct)
 
 export default router
